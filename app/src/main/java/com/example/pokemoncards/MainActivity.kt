68310a2208d315a2acc9102c668c244b2e5e9d66
Foo Tang <godfoo@gmail.com>
2023-11-02 22:58:10 -0500

    Working search added.

--- app/src/main/java/com/example/pokemoncards/MainActivity.kt
+++ app/src/main/java/com/example/pokemoncards/MainActivity.kt
@@ -3,36 +3,44 @@ package com.example.pokemoncards
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
 import com.example.pokemoncards.ui.theme.PokemonCardsTheme
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,8 +51,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //MyApp()
-                    MyAppPortrait()
+                    SearchScreen()
+                    //MyAppPortrait()
                 }
             }
         }
@@ -52,22 +60,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(){
+fun SearchScreen(){
 
     var cards by remember { mutableStateOf<List<Data>>(emptyList()) }
+//    var mainquery by remember {
+//        mutableStateOf("")
+//    }
 
-    CardList(cards)
+    Column {
+        SearchBar(onSearch = { newCards -> cards = newCards as List<Data> })
 
-    LaunchedEffect(true){
-        val result = PokemonApi.getCard()
-
-        if (result != null) {
-            cards = result.data
-        }
+        CardList(cards)
     }
-
-
+//    LaunchedEffect(mainquery != ""){
+//        val result = PokemonApi.getCard(mainquery)
+//
+//        if (result != null) {
+//            cards = result.data
+//        }
+//    }
 }
+
 @Composable
 fun CardList(cards: List<Data>){
 
@@ -84,8 +97,52 @@ fun CardList(cards: List<Data>){
             )
             Text(text = card.id)
             Text(text = card.name)
-            Text(text = card.rarity)
-            Text(text = card.tcgplayer.updatedAt)
+            card.rarity?.let { Text(text = it) }
+            card.tcgplayer?.let { Text(text = it.updatedAt) }
         }
     }
+}
+
+@Composable
+fun SearchBar(
+    onSearch: (Any?) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val focusManager = LocalFocusManager.current
+
+    var query by remember { mutableStateOf("") }
+
+    val corountine = rememberCoroutineScope()
+
+    TextField(
+        value = query,
+        onValueChange = {query = it},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions (onSearch = {
+            corountine.launch{
+                val result = PokemonApi.getCard(query)
+
+                if (result != null) {
+                    onSearch(result.data)
+                }
+            }
+            focusManager.clearFocus()
+        }),
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
 }
--- app/src/main/java/com/example/pokemoncards/PokemonApi.kt
+++ app/src/main/java/com/example/pokemoncards/PokemonApi.kt
@@ -1,11 +1,13 @@
 package com.example.pokemoncards
 
+import androidx.compose.runtime.Composable
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.appendPathSegments
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -24,16 +26,21 @@ class PokemonApi {
 
             }
         }
-        suspend fun getCard(): Main? {
 
-//            var result: Main = Main(null)
+        suspend fun getCard(search: String): Main? {
 
             val response: HttpResponse =
-                client.get("https://api.pokemontcg.io/v2/cards?q=name:charizard") {}
-//            if (response.status.value in 200..299) {
-//                 result = response.body()
-//            }
-            return response.body()
+                client.get("https://api.pokemontcg.io/v2/cards?q=name:$search") {
+                    url{
+                        //appendPathSegments("v2", "cards")
+                        //parameters.append("name", "Pikachu")
+                    }
+                }
+            return if (response.status.value in 200..299) {
+                response.body()
+            } else{
+                null
+            }
         }
 
     }
