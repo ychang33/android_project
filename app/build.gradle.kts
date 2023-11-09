ed38fe341f808e3e2b9be1df1718ac4060c89a96
Ivan Lai <ivan.lai.news@gmail.com>
2023-11-08 21:58:27 -0600

    Made the sign function work with firestore.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.android.material:material:1.10.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/pokemoncards/MainActivity.kt
+++ app/src/main/java/com/example/pokemoncards/MainActivity.kt
@@ -17,7 +17,9 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -28,6 +30,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -57,19 +60,33 @@ import dagger.hilt.android.AndroidEntryPoint
 import com.example.pokemoncards.navigation.Screen.SignInScreen
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.SubcomposeAsyncImage
+import com.example.pokemoncards.core.Utils
 import com.example.pokemoncards.destinations.CardDetailDestination
+import com.example.pokemoncards.destinations.LoginScreenDestination
 import com.example.pokemoncards.destinations.SearchScreenDestination
+import com.example.pokemoncards.screens.SignIn
+import com.example.pokemoncards.screens.SignInContent
+import com.example.pokemoncards.screens.SignInViewModel
 import com.example.pokemoncards.ui.theme.PokemonCardsTheme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 
 @AndroidEntryPoint
@@ -77,6 +94,7 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     private lateinit var navController: NavHostController
     private val viewModel by viewModels<MainViewModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -124,7 +142,8 @@ class MainActivity : ComponentActivity() {
 fun HomeScreen(
     navigator: DestinationsNavigator
 ){
-    navigator.navigate(SearchScreenDestination)
+    //navigator.navigate(SearchScreenDestination)
+    navigator.navigate(LoginScreenDestination)
 }
 
 @Destination
@@ -210,6 +229,124 @@ fun SearchBar(
             }
             focusManager.clearFocus()
         }),
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+@Destination
+@Composable
+fun LoginScreen(destinationsNavigator: DestinationsNavigator) {
+    val context = LocalContext.current
+    var isLoginSuccessful by remember { mutableStateOf(false)}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(Modifier.height(16.dp))
+        if (isLoginSuccessful){
+            // display UI is login success
+        }else{
+            LoginSection(Modifier.padding(horizontal = 16.dp), destinationsNavigator) { isSuccess ->
+                if (isSuccess){
+                    isLoginSuccessful = true
+                }else
+                {
+                    Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun LoginSection(
+    modifier: Modifier = Modifier, destinationsNavigator: DestinationsNavigator, onLoginClick:(Boolean) -> Unit
+) {
+    var userid by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier) {
+        Text(
+            text = "Pokémon Card Look Up",
+            modifier = modifier
+                .padding(top = 24.dp, bottom = 24.dp)
+        )
+        Spacer(Modifier.height(16.dp))
+        MyTextField(modifier, aValue = userid,
+            onChange = {userid = it},
+            aLabel = stringResource(R.string.placeholder_user_id),
+            aKeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            aVisualTransformation = VisualTransformation.None)
+        Spacer(Modifier.height(16.dp))
+        MyTextField(modifier, aValue = password,
+            onChange = {password = it},
+            aLabel = stringResource(R.string.placeholder_password),
+            aKeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            aVisualTransformation = PasswordVisualTransformation() )
+        Spacer(Modifier.height(16.dp))
+        Button(
+            onClick = {
+
+                val db = Firebase.firestore
+                val userRef = db.collection("users").document(userid)
+
+                userRef.get()
+                    .addOnSuccessListener { document ->
+                        if (document != null) {
+                            val userEmail = document.data?.get("email")
+                            val userPassword = document.data?.get("password")
+                            //Toast.makeText(context, "data: ${document.data?.get("password")}", Toast.LENGTH_SHORT).show()
+                            if (userPassword == password) {
+                                Toast.makeText(context, "Login success", Toast.LENGTH_SHORT).show()
+//                                destinationsNavigator.navigate(SearchScreenDestination(1, card)
+                                destinationsNavigator.navigate(SearchScreenDestination)
+                            }else
+                            {
+                                //Toast.makeText(context, "Login fail: ${document.data}", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(context, "Login fail", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            Toast.makeText(context, "User not found", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Toast.makeText(context, "Fail to locate the document", Toast.LENGTH_SHORT).show()
+                    }
+            } ){
+            Text("Login")
+        }
+
+    }
+}
+
+@Composable
+fun MyTextField(modifier: Modifier = Modifier,
+                aValue: String,
+                onChange: (String) -> Unit,
+                aLabel: String,
+                aVisualTransformation: VisualTransformation,
+                aKeyboardOptions: KeyboardOptions) {
+    TextField(
+        value = aValue,
+        onValueChange = onChange,
+        label = { Text(aLabel) },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        visualTransformation = aVisualTransformation,
+        keyboardOptions = aKeyboardOptions,
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 56.dp)
